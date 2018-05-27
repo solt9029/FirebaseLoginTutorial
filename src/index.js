04198a1ee0759310fad5b9e35390591b73db3105
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-05-27 17:46:11 +0900

    redux initial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
